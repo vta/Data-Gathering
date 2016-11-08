4a9942346164935dbb0a9a35647cbe5d8fe2f5e5
Vivek <3vivekb@gmail.com>
2016-11-07 21:33:50 -0800

    Update Web_Scrape.py

--- Web_Scrape.py
+++ Web_Scrape.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 url_to_scrape = 'http://data.vitalsigns.mtc.ca.gov/api/t5/flows/'
 r = requests.get(url_to_scrape)
-soup = BeautifulSoup(r.text)
-print (soup.get_text())
+pd.DataFrame(r.json())
